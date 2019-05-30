ec8be4d50f5fac221f08f4f3bab8583b1651d6f9
daodantiantou <1049297772@qq.com>
2019-05-30 11:28:07 +0800

    aa

--- stopCar/js/index.js
+++ stopCar/js/index.js
@@ -1,1 +1,26 @@
+window.onload=function(){
+	// 页面加载完成后执行
+	let $time=$('.time')
+	let $date=$('.date')
+	function updateTime(){
+		let date=new Date()
+		let nowdate=date.getFullYear()+'年'+((date.getMonth()+1)<10?'0'+(date.getMonth()+1):(date.getMonth()+1))+'月'+(date.getDate()<10?'0'+date.getDate():date.getDate())+'日'
+		$date.html(nowdate)
+		let h=date.getHours()
+		let m=date.getMinutes()
+		let s=date.getSeconds()
+		let nowtime=(h<10?'0'+h:h)+':'+(m<10?'0'+m:m)+':'+(s<10?'0'+s:s)
+		$time.html(nowtime)
+	}
+	let t=setInterval(updateTime,1000)
+	 
 
+
+
+
+
+
+
+
+	
+}
